963ab2b796236c740b0f9ec0cff950871e725344
bgmirko <bgmirko@hotmail.com>
2023-06-14 12:56:07 +0200

    setup database

---
+++ user-management-be/src/modules/user/user.entity.ts
@@ -0,0 +1,20 @@
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
+
+@Table
+export class User extends Model {
+  @Column({
+    type: DataType.UUID,
+    primaryKey: true,
+    defaultValue: DataType.UUIDV4,
+  })
+  uuid: string;
+
+  @Column
+  fullName: string;
+
+  @Column
+  password: string;
+
+  @Column
+  email: string;
+}
--- user-management-be/src/modules/user/user.service.ts
+++ user-management-be/src/modules/user/user.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @Inject('USERS_REPOSITORY')
+    private usersRepository: typeof User,
+  ) {}
+
+  async getUsers(query): Promise<{ count: number; rows: User[] }> {
+    return this.usersRepository.findAndCountAll({
+      attributes: { exclude: ['password'] },
+      offset: query?.cursor ?? 0,
+      limit: query?.limit ?? 10,
+    });
+  }
+}
